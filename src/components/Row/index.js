3f8d832f9f57d9487cb90167d11020ab48f4b566
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-05-08 11:10:32 -0700

    initial commit

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -4,14 +4,16 @@ import "./style.css"
 export default function Row(props){
 
     const hits = props.player.singles + props.player.doubles + props.player.triples+ props.player.home_runs;
-    const average = hits /3 ;
+    const average = hits/props.player.at_bats
     return (
         <tr>
             <td>{props.player.name}</td>
             <td>{props.player.team}</td>
             <td>{props.player.at_bats}</td>
             <td>{props.player.singles + props.player.doubles + props.player.triples+ props.player.home_runs}</td>
-            <td>{props.player.name}</td>
+            <td>{hits}</td>
+            <td>{hits}</td>
+            <td>{hits}</td>
 
 
         </tr>
